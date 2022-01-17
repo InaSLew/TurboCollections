138e7198ba149630e8abc40494216f93b7e7d1cc
InaSLew <ina.shenlewander@gmail.com>
2022-01-17 23:05:20 +0100

    Add and Get implementations and tests

--- TurboCollections.Test/UnitTest1.cs
+++ TurboCollections.Test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace TurboCollections.Test
@@ -11,12 +12,33 @@ namespace TurboCollections.Test
             Assert.Zero(list.Count);
         }
 
-        // [Test]
-        // public void AddShouldIncreaseCountToOne()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(5);
-        //     Assert.AreEqual(1, list.Count);
-        // }
+        [Test]
+        public void AddShouldIncreaseCountToOne()
+        {
+            var list = new TurboList<int>();
+            list.Add(5);
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [Test]
+        public void GetWithValidIndexShouldReturnItem()
+        {
+            var list = new TurboList<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(5);
+            var find = list.Get(4);
+            Assert.AreEqual(5, find);
+        }
+
+        [Test]
+        public void GetWithInvalidIndexShouldThrowException()
+        {
+            var list = new TurboList<int>();
+            list.Add(1);
+            Assert.Throws<IndexOutOfRangeException>(delegate { list.Get(10); });
+        }
     }
 }
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Collections;
 
 namespace TurboCollections
 {
     public class TurboList<T>
     {
-        public int Count => 5;
+        private T[] items;
+        
+        // returns the current amount of items contained in the list.
+        public int Count => items.Length;
+        
+        // adds one item to the end of the list.
+        public void Add(T item)
+        {
+            // Need to re-write this using the re-populating method
+            Array.Resize(ref items, Count + 1);
+            items[Count - 1] = item;
+        }
+        
+        // gets the item at the specified index. If the index is outside the correct range, an exception is thrown.
+        public T Get(int index)
+        {
+            if (index < 0 || index > Count - 1) throw new IndexOutOfRangeException();
+            return items[index];
+        }
+        
+        // // removes all items from the list.
+        // void Clear();
+        
+        // // removes one item from the list. If the 4th item is removed, then the 5th item becomes the 4th, the 6th becomes the 5th and so on.
+        // void RemoveAt(int index);
+        // // returns true, if the given item can be found in the list, else false.
+        // bool Contains(T item);
+        
+        // // returns the index of the given item if it is in the list, else -1.
+        // int IndexOf(T item);
+        
+        // // removes the specified item from the list, if it can be found.
+        // void Remove(T item);
+        
+        // // adds multiple items to this list at once.
+        // void AddRange(IEnumerable<T> items);
+        
+        // // gets the iterator for this collection. Used by IEnumerator to support foreach.
+        // IEnumerator<T>.GetEnumerator();
+        
         public TurboList()
         {
             Console.WriteLine("Hello Turbo!");
+            items = Array.Empty<T>();
         }
     }
 }
