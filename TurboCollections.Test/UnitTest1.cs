fbc6a8ea625f0b7f7122d0b8ff5ed0763b8477ab
InaSLew <ina.shenlewander@gmail.com>
2022-01-18 17:20:22 +0100

    Rest of them and the tests in

--- TurboCollections.Test/UnitTest1.cs
+++ TurboCollections.Test/UnitTest1.cs
@@ -132,111 +132,110 @@ namespace TurboCollections.Test
             Assert.AreEqual(list.Get(1), 2);
         }
         
-        // [Test]
-        // public void RemoveAtWithInvalidIndexShouldChangeItemIndex()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(3);
-        //     list.Add(2);
-        //     list.Add(1);
-        //     Assert.Throws<IndexOutOfRangeException>(delegate { list.RemoveAt(5); });
-        // }
-        //
-        // [Test]
-        // public void ContainShouldReturnTrueWhenFindExist()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(-1);
-        //     list.Add(500);
-        //     list.Add(3);
-        //     list.Add(50);
-        //     Assert.IsTrue(list.Contains(3));
-        // }
-        //
-        // [Test]
-        // public void ContainShouldReturnFalseWhenFindNotExist()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(6);
-        //     Assert.IsFalse(list.Contains(100));
-        // }
-        //
-        // [Test]
-        // public void IndexOfWithValidItemShouldReturnCorrectIndex()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(-1);
-        //     list.Add(500);
-        //     list.Add(3);
-        //     list.Add(50);
-        //     Assert.AreEqual(1, list.IndexOf(500));
-        // }
-        //
-        // [Test]
-        // public void IndexOfWithInvalidItemShouldReturnNonMinus1()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(-1);
-        //     list.Add(500);
-        //     list.Add(3);
-        //     list.Add(50);
-        //     Assert.AreEqual(-1, list.IndexOf(666));
-        // }
-        //
-        // [Test]
-        // public void RemoveWithValidItemShouldRemove()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(-1);
-        //     list.Add(500);
-        //     list.Add(3);
-        //     list.Add(50);
-        //     
-        //     list.Remove(3);
-        //     
-        //     Assert.AreEqual(3, list.Count); // length of list should one short
-        //     Assert.IsFalse(list.Contains(3)); // should no longer contain the removed item
-        // }
-        //
-        // [Test]
-        // public void RemoveWithInvalidItemShouldThrowException()
-        // {
-        //     var list = new TurboList<int>();
-        //     list.Add(-1);
-        //     list.Add(500);
-        //     list.Add(3);
-        //     list.Add(50);
-        //
-        //     Assert.Throws<InvalidOperationException>(delegate { list.Remove(666); });
-        // }
-        //
-        // [Test]
-        // public void AddRangeMultipleShouldWork()
-        // {
-        //     IEnumerable<int> itemsToAdd = new[] {1, 2, 3, 4};
-        //     var list = new TurboList<int>();
-        //     list.Add(3);
-        //     list.Add(2);
-        //     list.Add(1);
-        //     list.AddRange(itemsToAdd);
-        //     
-        //     Assert.AreEqual(TurboList<int>.BufferSize * 2, list.Count);
-        //     Assert.IsTrue(list.Contains(4));
-        //     Assert.AreEqual(6, list.IndexOf(4));
-        // }
-        //
-        // [Test]
-        // public void AddRangeSingleShouldWork()
-        // {
-        //     IEnumerable<int> itemsToAdd = new[] {666};
-        //     var list = new TurboList<int>();
-        //     list.Add(3);
-        //     list.Add(2);
-        //     list.Add(1);
-        //     list.AddRange(itemsToAdd);
-        //     
-        //     Assert.AreEqual(4, list.Count);
-        //     Assert.IsTrue(list.Contains(666));
-        // }
+        [Test]
+        public void RemoveAtWithInvalidIndexShouldChangeItemIndex()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+            Assert.Throws<IndexOutOfRangeException>(delegate { list.RemoveAt(5); });
+        }
+        
+        [Test]
+        public void ContainShouldReturnTrueWhenFindExist()
+        {
+            var list = new TurboList<int>();
+            list.Add(-1);
+            list.Add(500);
+            list.Add(3);
+            list.Add(50);
+            Assert.IsTrue(list.Contains(3));
+        }
+        
+        [Test]
+        public void ContainShouldReturnFalseWhenFindNotExist()
+        {
+            var list = new TurboList<int>();
+            list.Add(6);
+            Assert.IsFalse(list.Contains(100));
+        }
+        
+        [Test]
+        public void IndexOfWithValidItemShouldReturnCorrectIndex()
+        {
+            var list = new TurboList<int>();
+            list.Add(-1);
+            list.Add(500);
+            list.Add(3);
+            list.Add(50);
+            Assert.AreEqual(1, list.IndexOf(500));
+        }
+        
+        [Test]
+        public void IndexOfWithInvalidItemShouldReturnNonMinus1()
+        {
+            var list = new TurboList<int>();
+            list.Add(-1);
+            list.Add(500);
+            list.Add(3);
+            list.Add(50);
+            Assert.AreEqual(-1, list.IndexOf(666));
+        }
+        
+        [Test]
+        public void RemoveWithValidItemShouldRemove()
+        {
+            var list = new TurboList<int>();
+            list.Add(-1);
+            list.Add(500);
+            list.Add(3);
+            list.Add(50);
+            
+            list.Remove(3);
+            
+            Assert.AreEqual(3, list.Count); // length of list should one short
+            Assert.IsFalse(list.Contains(3)); // should no longer contain the removed item
+        }
+        
+        [Test]
+        public void RemoveWithInvalidItemShouldThrowException()
+        {
+            var list = new TurboList<int>();
+            list.Add(-1);
+            list.Add(500);
+            list.Add(3);
+            list.Add(50);
+        
+            Assert.Throws<InvalidOperationException>(delegate { list.Remove(666); });
+        }
+        
+        [Test]
+        public void AddRangeMultipleShouldWork()
+        {
+            IEnumerable<int> itemsToAdd = new[] {1, 2, 3, 4};
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+            
+            list.AddRange(itemsToAdd);
+            
+            Assert.AreEqual(7, list.Count);
+        }
+        
+        [Test]
+        public void AddRangeSingleShouldWork()
+        {
+            IEnumerable<int> itemsToAdd = new[] {666};
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+            list.AddRange(itemsToAdd);
+            
+            Assert.AreEqual(4, list.Count);
+            Assert.IsTrue(list.Contains(666));
+        }
     }
 }
